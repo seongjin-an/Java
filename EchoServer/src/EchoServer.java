efd98206da48dee12c1f34794e5f33555c11ae71
seongjin-an <seongjin4us@gmail.com>
2021-11-12 23:41:08 +0900

    netty

--- EchoServer/src/EchoServer.java
+++ EchoServer/src/EchoServer.java
@@ -7,6 +7,10 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class EchoServer {
     public static void main(String[] args) {
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
--- EchoServer/src/EchoServerHandler.java
+++ EchoServer/src/EchoServerHandler.java
@@ -1,8 +1,12 @@
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -14,8 +18,25 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 //        String readMessage = ((ByteBuf) o).toString(Charset.defaultCharset());
 //        channelHandlerContext.write(readMessage);
 //    }
+    public static ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        Channel incoming = ctx.channel();
+        for (Channel channel : channels) {
+            String response = "[SERVER] -"+incoming. remoteAddress ()+ "Add\n";
+            ByteBuf messageBuffer = Unpooled.buffer();
+            messageBuffer.writeBytes(response.getBytes(StandardCharsets.UTF_8));
+            //ctx.write("SUCCESS");//XXXX
+            channel.writeAndFlush(messageBuffer);
+//            channel.writeAndFlush ("[SERVER] -"+incoming. remoteAddress ()+ "Add\n");
+        }
+        channels.add(ctx.channel());
+    }
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
         String readMessage = ((ByteBuf) msg).toString(Charset.defaultCharset());
         System.out.println("수신한 문자열[" + readMessage + "]");
         String responseMessage = "awesome!!";
@@ -24,7 +45,6 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
         messageBuffer.writeBytes(responseMessage.getBytes(StandardCharsets.UTF_8));
         //ctx.write("SUCCESS");//XXXX
         ctx.write(messageBuffer);
-
     }
 
     @Override
